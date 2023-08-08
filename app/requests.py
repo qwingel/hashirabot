5ae317da08c1a05076e99795d55b85c6626cad2b
Antarktida <93553146+qwingel@users.noreply.github.com>
2023-08-08 11:34:08 +0300

    Add files via upload

--- app/requests.py
+++ app/requests.py
@@ -2,9 +2,39 @@ from flask import Blueprint, request, session
 
 module = Blueprint("main", __name__, url_prefix="/")
 
+players_data = ['' for i in range(32)]
 
 @module.post("/")
 def index():
     data = request.get_json()
-    print(data["login"])
-    return "Success! =)"
+
+    nickname = data["nick"]
+    SteamID = "https://steamcommunity.com/profiles/" + data["SteamID"]
+    result = nickname + ':' + SteamID
+
+    if result not in players_data:
+        for i in range(len(players_data)):
+            if(players_data[i] == ''):
+                players_data[i] = result
+
+    print(data)
+    return result
+
+@module.post("/delete")
+def clear_data():
+    data = request.get_json()
+
+    nickname = data["nick"]
+    SteamID = "https://steamcommunity.com/profiles/" + data["SteamID"]
+    result = nickname + ':' + SteamID
+    if result in players_data:
+        for i in range(len(players_data)):
+            if(players_data[i] == result):
+                players_data[i] = ''
+    
+    print(data)
+    return result
+
+def get_players_data():
+    players = players_data
+    return players
